86684271d01c785b878d536e6179a0eb5c439bf0
SliderOnTheBlack <105185690+SliderOnTheBlack@users.noreply.github.com>
2023-08-28 10:05:48 +0930

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
 	name: 'Elias',
 	imageBackground: false,
 	openInNewTab: true,
-	twelveHourFormat: false,
+	twelveHourFormat: true,
 
 	// Greetings
 	greetingMorning: 'Hello There!',
