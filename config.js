f9acda2d2d0a5216c21a42f679b011b008ab5d40
SliderOnTheBlack <105185690+SliderOnTheBlack@users.noreply.github.com>
2023-08-29 12:08:10 +0930

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '5ff9edf994mshcc22e8402ca4348p182a0ajsn4f8fcd088ea1', // Write here your API Key
+	weatherKey: 'bffa12d920bedf073db4be5f48d1473f', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
